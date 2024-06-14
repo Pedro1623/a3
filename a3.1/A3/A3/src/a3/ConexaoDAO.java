0b9aa16a2dc3122e96bec0d2fdfcc9fa04297f4d
Pedro1623 <101992276+Pedro1623@users.noreply.github.com>
2024-06-13 22:41:01 -0300

    ConexaoDAO.java

--- a3.1/A3/A3/src/a3/ConexaoDAO.java
+++ a3.1/A3/A3/src/a3/ConexaoDAO.java
@@ -1,15 +1,19 @@
 
 package a3;
 
-import com.sun.jdi.connect.spi.Connection;
-import javax.swing.JOptionPane;
+//import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
+//import javax.swing.JOptionPane;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoDAO {
+
+    /*Codigo do grupo antigo
     public Connection conectarBD(){
     Connection conn = null;
         try {
+            
             String url = "jdbc:mysql:\\localhost:3306/a3?user=root&password=";
             conn = (Connection) DriverManager.getConnection(url);
             
@@ -19,5 +23,9 @@ public class ConexaoDAO {
         return conn;
     }
     
-    
+     */
+    //codigo novo 
+    public static Connection conectarBD() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/crudea3");
+    }
 }
